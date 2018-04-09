112aa12ea8419b868ddb5c85fe0cba9d47e213e0
Javed Wright <jdwright870@gmail.com>
2018-04-09 17:17:56 +0000

    added upload form

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # INFO3180 Lab 7
-Starter code for Lab 7
+The purpose of this lab is for you to build an upload form with VueJS and
+submit it, sending the data via an AJAX request to be processed on the
+backend.
--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
 
+ALLOWED_IMAGES = set("jpg", "jpeg", "png")
+UPLOAD_FOLDER = '/static/uploads'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_IMAGES'] = ALLOWED_IMAGES
+
+app.config.from_object(__name__)
 from app import views
